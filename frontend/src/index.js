491d3572439b11857d66b08661ece1aec4ec2902
siripongn <siripong.ut.n@gmail.com>
2021-09-22 09:03:22 +0700

    9-th commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 //import AppTest from './AppTest';
+//import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
